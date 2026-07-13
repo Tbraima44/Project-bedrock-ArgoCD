df846ed1baa23a32ae89f866ff3b26b7ff7b7adf
Tbraima44 <brahimtoyheeb@gmail.com>
2026-07-13 19:42:24 +0100

    decrease the number of EKS nodes and switch to valueFiles for ArgoCD auto-detection

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -82,7 +82,7 @@ resource "aws_eks_node_group" "main" {
   subnet_ids      = aws_subnet.private[*].id
   
   scaling_config {
-    desired_size = 4
+    desired_size = 3
     max_size     = 5
     min_size     = 2
   }
